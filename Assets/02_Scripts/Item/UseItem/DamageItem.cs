a1689222d218a41c62b8f569015fa7e05021f8ca
YoonWooJoong <dbsdnwnd1126@gmail.com>
2025-04-23 12:18:48 +0900

    wip
    wip

--- Assets/02_Scripts/Item/UseItem/DamageItem.cs
+++ Assets/02_Scripts/Item/UseItem/DamageItem.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -8,13 +9,16 @@ public class DamageItem : BaseItem
 {
     private Tile _curTile;
     private ItemData _data;
+    private Action _handler;
+    public GameObject prefab;
+    public ProjectileItem controller;
 
     public override void AddItem(ItemData data)
     {
         if(_curTile.CharacterStatsOnTile is PlayerStats)
         {
             _curTile.itemsOnTile.Remove(this.gameObject.GetComponent<DropItem>());
-            _curTile.onCharacterChanged -= () => AddItem(data);
+            _curTile.onCharacterChanged -= _handler;
             Destroy(this.gameObject);
         }
     }
@@ -24,7 +28,9 @@ public class DamageItem : BaseItem
         _curTile = GameManager.Instance.PlayerTransform.curTile;
         _curTile.itemsOnTile.Add(this.gameObject.GetComponent<DropItem>());
         Debug.Log("아이템 버려짐");
-        _curTile.onCharacterChanged += () => AddItem(data);
+        _data = data;
+        _handler = () => AddItem(_data);
+        _curTile.onCharacterChanged += _handler;
     }
 
     /// <summary>
@@ -32,6 +38,15 @@ public class DamageItem : BaseItem
     /// </summary>
     public override void UseItem(ItemData data)
     {
+        //GameObject Prefa = Instantiate(prefab);
+        //controller = Prefa.GetComponent<ProjectileItem>();
+        //controller.Init(ResourceManager.Instance.dicItemData[200002], Instantiate(ResourceManager.Instance.typeObjectPrefab, this.transform).GetComponent<DamageItem>());
+        //if(TestTileManger.Instance.curLevel.tiles.TryGetValue(new Vector2Int(2, 0), out Tile tile))
+        //{
+        //    controller.projectileMove.SetDestination(tile);
+        //    controller.projectileMove.Init();
+        //}
+
         Vector2Int curPos = Vector2Int.RoundToInt(transform.position);
         List<Tile> tiles;
         if (data.target_range == 0 && data.effect_type == Effect_Type.Damage)
