78fc7ed037c562e4989e8e97337caa4e223d80c6
sungmars <136216542+sungmars@users.noreply.github.com>
2025-04-24 11:39:29 +0900

    🚧 [ chore ] 프리팹 변경사항 저장

--- Assets/02_Scripts/Item/Projectile/TESTESTSETS.cs
+++ Assets/02_Scripts/Item/Projectile/TESTESTSETS.cs
@@ -1,20 +1,20 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+//using System.Collections;
+//using System.Collections.Generic;
+//using UnityEngine;
 
-public class TESTESTSETS : MonoBehaviour
-{
-    public GameObject prefab;
-    public ProjectileItem controller;
-    public DropItem controller2;
-    public DropItem drop;
-    public GameObject dropPrefab;
-    // Start is called before the first frame update
-    void Start()
-    {
-        ItemManager.Instance.CreateDropItem(Instantiate(ResourceManager.Instance.typeObjectPrefab, this.transform).GetComponent<DamageItem>());
+//public class TESTESTSETS : MonoBehaviour
+//{
+//    public GameObject prefab;
+//    public ProjectileItem controller;
+//    public DropItem controller2;
+//    public DropItem drop;
+//    public GameObject dropPrefab;
+//    // Start is called before the first frame update
+//    void Start()
+//    {
+//        ItemManager.Instance.CreateDropItem(Instantiate(ResourceManager.Instance.typeObjectPrefab, this.transform).GetComponent<DamageItem>());
 
 
-    }
+//    }
 
-}
+//}
