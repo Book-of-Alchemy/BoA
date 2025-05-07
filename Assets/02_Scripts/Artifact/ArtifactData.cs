586110430a3d7605eae41d640732412de88fcdcd
YoonWooJoong <dbsdnwnd1126@gmail.com>
2025-05-07 21:39:56 +0900

    feat 아티팩트 추가
    1. 아티팩트 다운로더에 추가
    2. 아티팩트 데이터와 데이터 배이스 추가
    3. 아티팩트 제이슨 추가
    4. 아티팩트 so 추가

---
+++ Assets/02_Scripts/Artifact/ArtifactData.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum Rarity
+{
+    Common,
+    Uncommon,
+    Rare,
+}
+
+[CreateAssetMenu(fileName = "ArtifactData",menuName = "Artifact/Artifact")]
+public class ArtifactData : ScriptableObject
+{
+    public int id;
+    public string name_kr;
+    public string name_en;
+    public string description;
+    public string icon_sprite_id;
+    public Sprite icon_sprite;
+    public Rarity rarity;
+}
