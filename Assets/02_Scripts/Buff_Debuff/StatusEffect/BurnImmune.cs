de002a4c85382fc03d6d86d111e1eef5158bc2b1
judelover27 <judelover27@gmail.com>
2025-05-09 10:20:17 +0900

    add

--- Assets/02_Scripts/Buff_Debuff/StatusEffect/BurnImmune.cs
+++ Assets/02_Scripts/Buff_Debuff/StatusEffect/BurnImmune.cs
@@ -1,18 +1,19 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class BurnImmune : MonoBehaviour
+public class BurnImmune : Immune
 {
-    // Start is called before the first frame update
-    void Start()
+    public BurnImmune(StatusEffectData data) : base(data, new HashSet<Type>() { typeof(Rooted) })
     {
-        
+        this.data = data;
     }
-
-    // Update is called once per frame
-    void Update()
+    public BurnImmune(StatusEffectData data, int value, int remainingTime, int tickInterval) : base(data, new HashSet<Type>() { typeof(Rooted) })
     {
-        
+        this.data = data;
+        this.value = value;
+        this.remainingTime = remainingTime;
+        this.tickInterval = tickInterval;
     }
 }
