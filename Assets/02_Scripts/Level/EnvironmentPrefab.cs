c9a9da74b3dbcbc2f2b8510631ba45020f64f848
judelover27 <judelover27@gmail.com>
2025-05-15 14:51:31 +0900

    wip

--- Assets/02_Scripts/Level/EnvironmentPrefab.cs
+++ Assets/02_Scripts/Level/EnvironmentPrefab.cs
@@ -6,9 +6,17 @@ using UnityEngine;
 public class EnvironmentPrefab : TilePrefab
 {
     public event Action OnReturnEvent;
+    public EnvironmentalData data;
     
     public void OnReturn()
     {
         OnReturnEvent?.Invoke();
+    }
+
+    public void AutoTileUpdate()
+    {
+        var neighbor = TileUtility.GetAdjacentTileList(CurTile.curLevel, CurTile);
+        foreach (var tile in neighbor) {
+
     }
 }
