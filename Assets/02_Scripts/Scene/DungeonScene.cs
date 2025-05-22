2c12b4b763973c141760ad0791c5aa3221309a1e
sangweon25 <seosangweon1290@gmail.com>
2025-05-22 21:01:19 +0900

    🔧 [ fix ] 던전씬 입장시 초기화

--- Assets/02_Scripts/Scene/DungeonScene.cs
+++ Assets/02_Scripts/Scene/DungeonScene.cs
@@ -12,20 +12,13 @@ public class DungeonScene : SceneBase
     {
         UIManager.Instance.RefreshUIList();
         // UI 확인 전에 UIManager가 준비되었는지 확인
-        if (UIManager.Instance != null)
+        if (!UIManager.IsOpened<UI_HUD>())
         {
-            try
-            {
-                if (!UIManager.IsOpened<UI_HUD>())
-                {
-                    UIManager.Show<UI_HUD>();
-                }
-            }
-            catch (System.Exception e)
-            {
-             
-            }
+            UIManager.Show<UI_HUD>();
         }
+        TurnManager.Instance.ResetManager();
+        TileManger.Instance.GenerateDungeon();
+        TurnManager.Instance.StartTurnCycle();
     }
 
     public override void OnExit()
